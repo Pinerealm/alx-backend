9f089373d5155c25148a5909e0cc68e818edd886
Pinerealm <osalami09@gmail.com>
2023-10-26 06:48:47 +0300

    Change attribute name from queue to stack

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -9,18 +9,18 @@ class LIFOCache(BaseCaching):
     def __init__(self):
         """Runs on instantiation"""
         super().__init__()
-        self.queue = []
+        self.stack = []
 
     def put(self, key, item):
         """Puts a key/value pair into cache"""
         if key and item:
             if key in self.cache_data:
-                self.queue.remove(key)
+                self.stack.remove(key)
             elif len(self.cache_data) >= self.MAX_ITEMS:
-                discard = self.queue.pop()
+                discard = self.stack.pop()
                 del self.cache_data[discard]
                 print("DISCARD: {}".format(discard))
-            self.queue.append(key)
+            self.stack.append(key)
             self.cache_data[key] = item
 
     def get(self, key):
