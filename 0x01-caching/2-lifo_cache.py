de99de4234fe36467886d1851a60b0e53650b477
Pinerealm <osalami09@gmail.com>
2023-10-26 06:37:33 +0300

    Implement a LIFO cache

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""LIFO Caching"""
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """LIFO Cache"""
+
+    def __init__(self):
+        """Runs on instantiation"""
+        super().__init__()
+        self.queue = []
+
+    def put(self, key, item):
+        """Puts a key/value pair into cache"""
+        if key and item:
+            if key in self.cache_data:
+                self.queue.remove(key)
+            elif len(self.cache_data) >= self.MAX_ITEMS:
+                discard = self.queue.pop()
+                del self.cache_data[discard]
+                print("DISCARD: {}".format(discard))
+            self.queue.append(key)
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Gets the value associated with given key"""
+        return self.cache_data.get(key)
